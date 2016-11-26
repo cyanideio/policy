29c9003ee8e3929714bf725e8d4ff660b917f86d
Frank <knarfytrebil@gmail.com>
2016-11-26 12:37:10 +0800

    remove policies

--- PolicyFactory.js
+++ PolicyFactory.js
@@ -3,12 +3,13 @@ const ErrorThrowerFactory = require('phoenix-error').ErrorThrowerFactory
 /** Builds Policies */
 class PolicyFactory {
 	constructor(opt){
+		this.policy_path = opt.policy_path
 		this.errorThrowerFactory = new ErrorThrowerFactory({
 			conf: opt.error_conf
 		})
 	}
 	build(policy_name) {
-		let Policy = require(`./policies/${policy_name}`)
+		let Policy = require(`${this.policy_path}/${policy_name}`)
 		return new Policy({
 			errorThrower: this.errorThrowerFactory.build(policy_name)	
 		})
