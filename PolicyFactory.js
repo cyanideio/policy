a8d5dcf8769b38a62d3503d7767e62715cd4990f
Frank <knarfytrebil@gmail.com>
2016-11-26 17:27:53 +0800

    fix

--- PolicyFactory.js
+++ PolicyFactory.js
@@ -2,18 +2,26 @@ const ErrorThrowerFactory = require('cyanide-error').ErrorThrowerFactory
 
 /** Builds Policies */
 class PolicyFactory {
+
 	constructor(opt){
 		this.policy_path = opt.policy_path
 		this.errorThrowerFactory = new ErrorThrowerFactory({
 			conf: opt.error_conf
 		})
 	}
+
+	/**
+	 * Build
+	 * @param  {String} policy_name [description]
+	 * @return {Policy}             [description]
+	 */
 	build(policy_name) {
-		let Policy = require(`${this.policy_path}/${policy_name}`)
+		let Policy = require(`../../${this.policy_path}/${policy_name}`)
 		return new Policy({
 			errorThrower: this.errorThrowerFactory.build(policy_name)	
 		})
 	}
+	
 }
 
 module.exports = PolicyFactory
